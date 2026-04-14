e2169bebafe26498ed64b519f25d7433fd8da6a4
test <test@test.com>
2026-04-14 17:03:16 +0800

    keep only argosbx.sh

--- _worker.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url)
-    url.protocol = 'https:'
-    url.hostname = '域名'
-    return fetch(new Request(url, request))
-  }
-}
